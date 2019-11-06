fd7aef9ecb89bd94ac9490423ad5ec0ae2ffd0bb
Brian Torres <luxormagnes@gmail.com>
2019-11-05 23:34:38 -0400

    Se crearon los métodos CRUD y conexión a BD

--- server/src/app.js
+++ server/src/app.js
@@ -1,17 +1,27 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const cors = require('cors')
-const morgan = require('morgan')
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+const app = express();
+const mongoose = require('mongoose');
+const URI = 'mongodb+srv://Sapientier:portafolio-ucv@portafolio-bd-3ovqh.mongodb.net/test?retryWrites=true&w=majority';
 
-const app = express()
-app.use(morgan('combined'))
-app.use(bodyParser.json())
-app.use(cors())
+// Conexión a la BD en la Nube Cloud Atlas
+mongoose.connect(URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(() => console.log("DB conectada"))
+    .catch(err => console.error(err));
 
-app.post('/register', (req, res) => {
-    res.send({
-        message: `Hello ${req.body.email}! Your user was registered. Have fun!`
-    })
-})
+// Middlewares -- Bloques de código que se ejecutan entre la petición que hace el usuario (request) hasta que la petición llega al servidor
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(cors());
 
-app.listen(process.env.PORT || 8000)
+// Rutas
+app.use('/tasks', require('./routes/tasks'));
+
+// Configuraciones del servidor
+app.set('port', process.env.PORT || 8000); // Que tome por defecto el puerto que se brinde donde esté desplegada la app, sino el 8000
+
+// Inicialización del servidor
+app.listen(app.get('port'), () => {
+    console.log(`Servidor inicializado en el puerto ${app.get('port')}`);
+});
