08f1f92ad75d261b35609c724ca052f060be77d7
Brian Torres <luxormagnes@gmail.com>
2020-12-01 13:45:57 -0400

    Ajustes a la pantalla de créditos

--- client/src/components/Credits.vue
+++ client/src/components/Credits.vue
@@ -1,31 +1,36 @@
 <template>
-  <v-container grid-list-xl mt-15>
-    <v-row wrap>
-      <v-col xs12 md6>
+  <v-container grid-list-xl mt-5>
+    <v-row justify="center">
+      <v-icon x-large>mdi-account-box-multiple</v-icon>
+    </v-row>
+    <v-row>
+      <v-col xs="12" md="6" cols="12">
         <v-card class="mx-auto elevation-6" max-width="600" outlined>
           <v-list-item>
             <v-list-item-content>
               <div class="overline mb-4">CRÉDITOS</div>
-              Este sitio web fue conceptualizado, diseñado y elaborado por los
-              estudiantes de computación Brian Torres y Jorge Ramirez, siendo su
-              directora la Prof. Yosly Hernandez.
+              Esta Aplicación Web fue desarrollada por los estudiantes de
+              computación Brian Torres y Jorge Ramirez como Trabajo Especial de
+              Grado para optar el título de Licenciado en Computacion de la UCV,
+              bajo la tutoría de la Profa. Yosly Hernández.
               <div class="mb-4"></div>
-              Este proyecto de “Aplicación Web Del Portafolio Digital Para La
-              Gestión Del Conocimiento De La Facultad De Ciencias De La
-              Universidad Central De Venezuela”, tiene como propósito el
-              refrescamiento y relanzamiento de la Imagen corporativa y la
-              visualización de los diferentes servicios que ofrece la Facultad
-              de Ciencias en sus distintas áreas de funcionamiento.
+              Este proyecto de “Portafolio Digital Ciencias UCV” tiene como
+              propósito el refrescamiento y relanzamiento de la Imagen
+              corporativa y la visualización de los diferentes servicios que
+              ofrece la Facultad de Ciencias en sus distintas áreas de
+              funcionamiento.
             </v-list-item-content>
           </v-list-item>
         </v-card>
       </v-col>
-      <v-col xs12 md6>
+      <v-col xs="12" md="6" cols="12">
         <v-card class="mx-auto elevation-6" max-width="600" outlined>
           <v-list-item>
             <v-list-item-content>
-              <v-list-item-title>Dirección:</v-list-item-title>
-              <v-list-item-subtitle>Prof. Yosly Hernandez</v-list-item-subtitle>
+              <v-list-item-title>Tutor:</v-list-item-title>
+              <v-list-item-subtitle
+                >Profa. Yosly Hernández</v-list-item-subtitle
+              >
             </v-list-item-content>
           </v-list-item>
           <v-list-item>
@@ -33,7 +38,9 @@
               <v-list-item-title
                 >Conceptualización, diagramación y contenido:</v-list-item-title
               >
-              <v-list-item-subtitle>Prof. Yosly Hernandez</v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Profa. Yosly Hernández</v-list-item-subtitle
+              >
               <v-list-item-subtitle>Prof. Antonio Machado</v-list-item-subtitle>
               <v-list-item-subtitle
                 >Prof. Ernesto Fuenmayor</v-list-item-subtitle
@@ -52,13 +59,47 @@
         </v-card>
       </v-col>
     </v-row>
+    <v-row justify="center">
+      <v-chip
+        class="ma-2"
+        color="indigo"
+        text-color="white"
+        @click="
+          navigateTo('https://www.linkedin.com/in/brian-torres-ba2a1b164')
+        "
+      >
+        <v-avatar left>
+          <v-icon>mdi-linkedin</v-icon>
+        </v-avatar>
+        Brian Torres
+      </v-chip>
+
+      <!-- <v-chip
+          class="ma-2"
+          color="indigo"
+          text-color="white"
+          @click="
+            navigateTo('')
+          "
+        >
+          <v-avatar left>
+            <v-icon>mdi-linkedin</v-icon>
+          </v-avatar>
+          Jorge Ramirez
+        </v-chip> -->
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   data: () => ({
-    //
+    model: -1,
   }),
+  methods: {
+    navigateTo(route) {
+      window.open(route, "_blank");
+    },
+  },
 };
 </script>
