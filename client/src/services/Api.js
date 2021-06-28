ecd74607b304d9cd135bf015d05662add9b8e9cd
Jorge Ramírez <jlr032032@gmail.com>
2021-06-28 10:33:09 -0400

    Desincorpora uso de variables de entorno en aplicación cliente

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
-        baseURL: process.env.VUE_APP_API_BASE_URL
+        baseURL: 'http://localhost:8000/'
     })
 }
