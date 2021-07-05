df6b528feca4d7ca8e30db7e2e8fa4150033d9e4
Brian Torres <luxormagnes@gmail.com>
2021-07-05 11:26:10 -0400

    Ajustando Banner

--- client/src/components/home/Banner.vue
+++ client/src/components/home/Banner.vue
@@ -5,7 +5,7 @@
                 <v-row align="center" justify="center">
                     <v-col class="text-center" cols="10">
                         <h1 class="display-3 font-weight-light">
-                            Portafolio Ciencias
+                            Portafolio Ciencias UCV
                         </h1>
                         <h4 class="font-weight-light mt-3 mb-3">
                             ¡Bienvenidos! Para aquellos problemas que requieren
@@ -19,35 +19,6 @@
                 </v-row>
             </v-overlay>
         </v-parallax>
-
-        <!-- <v-row class="white">
-            <v-col xs="12">
-                <v-carousel
-                    cycle
-                    height="400"
-                    hide-delimiter-background
-                    show-arrows-on-hover
-                >
-                    <v-carousel-item
-                        v-for="(item, i) in items"
-                        :key="i"
-                        :src="item.src"
-                    >
-                        <v-overlay :absolute="absolute" :opacity="opacity">
-                            <v-row
-                                class="fill-height"
-                                align="center"
-                                justify="center"
-                            >
-                                <div class="display-1 text-center">
-                                    <v-col xs="12">{{ item.slide }}</v-col>
-                                </div>
-                            </v-row>
-                        </v-overlay>
-                    </v-carousel-item>
-                </v-carousel>
-            </v-col>
-        </v-row> -->
     </base-card>
 </template>
 
@@ -57,26 +28,6 @@ export default {
         return {
             absolute: true,
             opacity: 0.65,
-            // items: [
-            //     {
-            //         slide: "Proceso de inscripcion I-2020",
-            //         src:
-            //             "https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg",
-            //     },
-            //     {
-            //         slide: "II Congreso de Transformación Digital",
-            //         src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
-            //     },
-            //     {
-            //         slide: "Reincorporaciones 1-2019",
-            //         src: "https://cdn.vuetifyjs.com/images/carousel/bird.jpg",
-            //     },
-            //     {
-            //         slide: "Cronograma de ingreso por Mérito Académico",
-            //         src: "https://cdn.vuetifyjs.com/images/carousel/planet.jpg",
-            //     },
-            // ],
-            // slides: ["Noticia 1", "Second", "Third", "Fourth", "Fifth"],
         };
     },
     computed: {
